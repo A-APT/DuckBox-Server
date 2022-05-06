52ff07746a3cfafb1755461433c9196eeeca53ce
0JIEUN0 <je300512@gmail.com>
2022-05-06 12:49:46 +0900

    feat(survey-controller): add support for generate survey token

--- src/main/kotlin/com/duckbox/controller/SurveyController.kt
+++ src/main/kotlin/com/duckbox/controller/SurveyController.kt
@@ -1,7 +1,9 @@
 package com.duckbox.controller
 
+import com.duckbox.dto.BlindSigToken
 import com.duckbox.dto.survey.SurveyDetailDto
 import com.duckbox.dto.survey.SurveyRegisterDto
+import com.duckbox.dto.user.BlingSigRequestDto
 import com.duckbox.security.JWTTokenProvider
 import com.duckbox.service.SurveyService
 import org.springframework.http.ResponseEntity
@@ -26,5 +28,11 @@ class SurveyController (
     @GetMapping("/api/v1/survey/group/{groupId}")
     fun findSurveysOfGroup(@RequestHeader httpHeaders: Map<String, String>, @PathVariable groupId: String): ResponseEntity<List<SurveyDetailDto>> {
         return surveyService.findSurveysOfGroup(groupId)
+    }
+
+    @PostMapping("/api/v1/survey/signatures")
+    fun generateSurveyToken(@RequestHeader httpHeaders: Map<String, String>, @RequestBody blindSigRequestDto: BlingSigRequestDto): ResponseEntity<BlindSigToken> {
+        val userEmail: String = jwtTokenProvider.getUserPK(jwtTokenProvider.getTokenFromHeader(httpHeaders)!!)
+        return surveyService.generateBlindSigSurveyToken(userEmail, blindSigRequestDto)
     }
 }
--- src/test/kotlin/com/duckbox/controller/SurveyControllerTest.kt
+++ src/test/kotlin/com/duckbox/controller/SurveyControllerTest.kt
@@ -1,17 +1,21 @@
 package com.duckbox.controller
 
 import BlindSecp256k1
+import BlindedData
+import com.duckbox.DefinedValue
 import com.duckbox.MockDto
 import com.duckbox.domain.group.GroupRepository
 import com.duckbox.domain.photo.PhotoRepository
 import com.duckbox.domain.survey.SurveyRepository
 import com.duckbox.domain.user.UserBoxRepository
 import com.duckbox.domain.user.UserRepository
+import com.duckbox.dto.BlindSigToken
 import com.duckbox.dto.group.GroupRegisterDto
 import com.duckbox.dto.user.LoginRequestDto
 import com.duckbox.dto.user.RegisterDto
 import com.duckbox.dto.survey.SurveyDetailDto
 import com.duckbox.dto.survey.SurveyRegisterDto
+import com.duckbox.dto.user.BlingSigRequestDto
 import com.duckbox.service.GroupService
 import com.duckbox.service.SurveyService
 import com.duckbox.service.UserService
@@ -30,6 +34,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.math.BigInteger
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension::class)
@@ -233,6 +238,74 @@ class SurveyControllerTest {
             .apply {
                 Assertions.assertThat(statusCode).isEqualTo(HttpStatus.OK)
                 Assertions.assertThat(body!!.size).isEqualTo(0)
+            }
+    }
+    
+    @Test
+    fun is_generateSurveyToken_works_no_authToken_header() {
+        // arrange
+        val httpEntity = HttpEntity(ObjectId().toString(), HttpHeaders())
+
+        // act, assert
+        restTemplate
+            .exchange("${baseAddress}/api/v1/survey/signatures", HttpMethod.POST, httpEntity, Unit::class.java)
+            .apply {
+                Assertions.assertThat(statusCode).isEqualTo(HttpStatus.FORBIDDEN)
+            }
+    }
+
+    @Test
+    fun is_generateSurveyToken_works_well() {
+        // arrange
+        val token: String = registerAndLogin()
+        val httpHeaders = HttpHeaders().apply {
+            this["Authorization"] = "Bearer $token"
+        }
+        val surveyId: String = surveyService.registerSurvey(mockUserEmail, MockDto.mockSurveyRegisterDto).body!!
+
+        val message: ByteArray = "test".encodeToByteArray()
+        val blindedData: BlindedData = blindSecp256k1.blind(DefinedValue.R_, message)
+        val blindSigRequestDto = BlingSigRequestDto(targetId = surveyId, blindMessage = blindedData.blindM.toString(16))
+        val httpEntity = HttpEntity(blindSigRequestDto, httpHeaders)
+
+        // act, assert
+        restTemplate
+            .exchange("${baseAddress}/api/v1/survey/signatures", HttpMethod.POST, httpEntity, BlindSigToken::class.java)
+            .apply {
+                Assertions.assertThat(statusCode).isEqualTo(HttpStatus.OK)
+                val surveyToken: BlindSigToken = body!!
+                val serverBSig: BigInteger = BigInteger(surveyToken.serverBSig, 16)
+                val ownerBSig: BigInteger = BigInteger(surveyToken.ownerBSig, 16)
+                val serverSig: BigInteger = blindSecp256k1.unblind(blindedData.a, blindedData.b, serverBSig)
+                val surveyOwnerSig: BigInteger = blindSecp256k1.unblind(blindedData.a, blindedData.b, ownerBSig)
+
+                Assertions.assertThat(blindSecp256k1.verify(serverSig, blindedData.R, message, DefinedValue.pubkey)).isEqualTo(true)
+                Assertions.assertThat(
+                    blindSecp256k1.verify(
+                        surveyOwnerSig,
+                        blindedData.R,
+                        message,
+                        DefinedValue.ownerPublic
+                    )
+                ).isEqualTo(true)
+            }
+    }
+
+    @Test
+    fun is_generateSurveyToken_works_on_invalid_user() {
+        // arrange
+        val token: String = registerAndLogin()
+        val httpHeaders = HttpHeaders().apply {
+            this["Authorization"] = "Bearer invalid_token"
+        }
+        val surveyId = surveyService.registerSurvey(mockUserEmail, MockDto.mockSurveyRegisterDto)
+        val httpEntity = HttpEntity(surveyId.toString(), httpHeaders)
+
+        // act, assert
+        restTemplate
+            .exchange("${baseAddress}/api/v1/survey/signatures", HttpMethod.POST, httpEntity, Unit::class.java)
+            .apply {
+                Assertions.assertThat(statusCode).isEqualTo(HttpStatus.FORBIDDEN)
             }
     }
 }
